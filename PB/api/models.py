4e22f748e8d745882884f6cad556542dd4593391
Entro <shubham64negi@gmail.com>
2024-03-11 13:43:58 +0530

    created databse model

--- PB/api/models.py
+++ PB/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Employee(AbstractUser):
+    EMPLOYEE_ROLES = (
+        ('AM', 'Admin'),
+        ('PC', 'Project Coordinator'),
+        ('FR', 'Freelancer'),
+        ('AC', 'Accounting'),
+    )
+
+    employee_id = models.CharField(max_length=15, unique=True)
+    role = models.CharField(max_length=2, choices=EMPLOYEE_ROLES, default='FR')
+    email = models.EmailField(unique=True)
+    contact_number = models.CharField(max_length=15)
+    address = models.TextField()
+    pincode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return self.employee_id
+
+    def save(self, *args, **kwargs):
+        # Extract the role code from the employee_id
+        self.role = self.employee_id[3:5]
+        super().save(*args, **kwargs)
